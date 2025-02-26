339ebf1744afa0d895b8ff048338916f262db08b
changjiahong <2327085154@qq.com>
2025-02-26 13:48:19 +0800

    |

--- composeApp/src/commonMain/kotlin/cn/changjiahong/bamb/bamb/rttext/RtText.kt
+++ composeApp/src/commonMain/kotlin/cn/changjiahong/bamb/bamb/rttext/RtText.kt
@@ -12,14 +12,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import cn.changjiahong.bamb.bamb.html.NodeHandlers
+import cn.changjiahong.bamb.bamb.html.handler.BNode
+import cn.changjiahong.bamb.bamb.html.handler.SpanNode
+import cn.changjiahong.bamb.bamb.html.traverseChild
 import com.fleeksoft.ksoup.Ksoup
 import com.fleeksoft.ksoup.nodes.Document
 import com.fleeksoft.ksoup.nodes.Node
 import com.fleeksoft.ksoup.nodes.TextNode
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
 
 @Composable
 fun demo() {
@@ -33,7 +41,7 @@ fun demo() {
         RtText(rtTextState)
 
         Button(onClick = {
-            rtTextState.contentText = "Click $num"
+            rtTextState.contentText = "<>"
         }) {
             Text("Click")
         }
@@ -98,16 +106,16 @@ class RtTextState(
 
 object AnnotatedStringParser {
 
-    fun parse(content: String, type: ParserType = ParserType.Html): AnnotatedString {
+    fun parse(content: String, type: ParserType = ParserType.Html,tagHandlers: TagHandlers = TagHandlers.DefaultHandlers): AnnotatedString {
         return when (type) {
-            ParserType.Html -> htmlParse(content)
+            ParserType.Html -> htmlParse(content,tagHandlers)
             ParserType.Markdown -> markdownParse(content)
         }
     }
 
-    private fun htmlParse(content: String): AnnotatedString {
+    private fun htmlParse(content: String, tagHandlers: TagHandlers = TagHandlers.DefaultHandlers): AnnotatedString {
         val doc = Ksoup.parse(content)
-        return AnnotatedStringBuilder(doc).build()
+        return AnnotatedStringBuilder(doc,tagHandlers).build()
     }
 
     private fun markdownParse(content: String): AnnotatedString {
@@ -117,7 +125,7 @@ object AnnotatedStringParser {
 
 class AnnotatedStringBuilder(
     val document: Document,
-    val handlers: NodeHandlers = NodeHandlers.DefaultHandlers
+    val tagHandlers: TagHandlers = TagHandlers.DefaultHandlers
 ) {
 
     private fun AnnotatedString.Builder.appendNode(node: Node) {
@@ -126,7 +134,13 @@ class AnnotatedStringBuilder(
                 append(node.text())
             }
 
-            else -> handlers(this, node)
+            else -> if (!tagHandlers(this, node)) appendChild(node)
+        }
+    }
+
+    private fun AnnotatedString.Builder.appendChild(node: Node) {
+        node.childNodes().forEach {
+            appendNode(it)
         }
     }
 
@@ -141,11 +155,87 @@ sealed interface ParserType {
     data object Markdown : ParserType
 }
 
-interface Tag {
-    object A : Tag
+abstract class Tag(val name: String) {
+    object NULL : Tag("")
+
+    object A : Tag("a")
+}
+private typealias AnnotatedStringNodeHandler = AnnotatedString.Builder.(node: Node) -> Unit
+
+open class TagHandler(val tag: Tag, val annotatedStringNodeHandler: AnnotatedStringNodeHandler) :
+        (AnnotatedString.Builder, Node) -> Boolean {
+    override fun invoke(builder: AnnotatedString.Builder, node: Node): Boolean = run {
+        builder.annotatedStringNodeHandler(node)
+        true
+    }
 }
 
-abstract class TagHandler(val tag: Tag)
+
+class TagHandlers : TagHandler(Tag.NULL, {}) {
+    private val tagHandlers = mutableMapOf<String, AnnotatedStringNodeHandler>()
+
+    fun addTagHandlers(vararg tagHandlers: TagHandler) {
+        tagHandlers.forEach { handler ->
+            addTagHandler(handler)
+        }
+    }
+
+    fun addTagHandler(tagHandler: TagHandler) {
+        addTagHandler(tagHandler.tag.name, tagHandler.annotatedStringNodeHandler)
+    }
+
+    fun addTagHandler(name: String, annotatedStringNodeHandler: AnnotatedStringNodeHandler) {
+        if (tagHandlers.containsKey(name)) {
+            return
+        }
+        tagHandlers[name] = annotatedStringNodeHandler
+    }
+
+
+    override fun invoke(builder: AnnotatedString.Builder, node: Node): Boolean {
+        if (tagHandlers.containsKey(node.nodeName())) {
+            tagHandlers[node.nodeName()]?.let { it(builder, node) }
+            return true
+        }
+        return false
+    }
+
+    companion object {
+        val DefaultHandlers: TagHandlers = TagHandlers()
+
+        init {
+            Handlers.apply {
+                DefaultHandlers.addTagHandlers(
+                    ATagHandler
+                )
+            }
+        }
+    }
+
+}
+
+object Handlers
+
+val Handlers.ATagHandler by tagHandler(Tag.A) { node: Node ->
+    withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+        traverseChild(node)
+    }
+}
+
+fun tagHandler(
+    tag: Tag,
+    nodeHandler: AnnotatedStringNodeHandler
+): ReadOnlyProperty<Handlers, TagHandler> {
+    return object : ReadOnlyProperty<Handlers, TagHandler> {
+        override fun getValue(
+            thisRef: Handlers,
+            property: KProperty<*>
+        ): TagHandler {
+            return TagHandler(tag, nodeHandler)
+        }
+    }
+}
+
 
 class ParserConfig(
 
